40cff39d54b7db01f76c013c69a1520e147a9350
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-02-24 16:03:15 +0000

    try

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeRange() {
+    var width = 500;
+    
+    var percent = document.getElementById('range-slider-input').value/10;
+    
+    if (percent > 95) percent -= 5;
+    if (percent < 5) percent += 5;
+    
+    document.getElementById('range-fill').style.width = percent+ '%';
+  }
+  
+  document.onload = changeRange();
